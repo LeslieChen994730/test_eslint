99d05133e414eef63de364a141d9da7a399793f5
chenwenliang <282113382@qq.com>
2020-06-16 23:37:01 +0800

    a

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,22 +5,20 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   env: {
     browser: true,
   },
   // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
   // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['prettier','plugin:vue/essential'],
+  extends: ['plugin:vue/essential', 'eslint:recommended', 'plugin:prettier/recommended'],
   // required to lint *.vue files
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['vue','prettier'],
   // add your custom rules here
   rules: {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': ['warn']
-  }
-}
+    'prettier/prettier': ['error'],
+  },
+};
--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app';
   router;
+
+
+
   components: { App },
   template: '<App/>',
 });
