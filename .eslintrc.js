e5d45c7f6b57eacca7f55b4a6fb8dd3061b1e909
chenwenliang <282113382@qq.com>
2020-06-16 22:51:08 +0800

    a

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     // allow debugger during development
-    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'prettier/prettier': ['warn']
   }
 }
